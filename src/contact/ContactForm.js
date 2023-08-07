664f3b5fd509b8e016eeb2c67cfe92ab697124ab
송현서 <songmin21s@songhyeonseoui-MacBookPro.local>
2023-08-07 17:12:06 +0900

    asdf

--- src/contact/ContactForm.js
+++ src/contact/ContactForm.js
@@ -1,5 +1,4 @@
 import React, { useState } from 'react'
-import axios from 'axios'
 import Swal from 'sweetalert2'
 import withReactContent from 'sweetalert2-react-content'
 const MySwal = withReactContent(Swal)
