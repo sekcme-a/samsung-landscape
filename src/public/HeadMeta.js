f73dc1ecb3af1c2bbb0e0773359f2837105d7b1c
송현서 <songmin21s@songhyeonseoui-MacBookPro.local>
2024-07-16 13:11:18 +0900

    asdf

--- src/public/HeadMeta.js
+++ src/public/HeadMeta.js
@@ -8,7 +8,7 @@ const HeadMeta = ({ title, description, url }) => {
         name="description"
         content={
           description ||
-          "더 나은 환경과 삶은 위한 풍요로움을 창출합니다. 조경 식재, 시설물 설치공사 등 창의적이고 지속 가능한 조경을 추구합니다."
+          "삼성조경은 조경 식재, 시설물 설치공사 등 창의적이고 지속 가능한 조경을 추구합니다."
         }
       />
       <meta name="viewport" content="initial-scale=1.0, width=device-width" />
@@ -16,7 +16,7 @@ const HeadMeta = ({ title, description, url }) => {
       <meta property="og:description"
         content={
         description ||
-        "더 나은 환경과 삶은 위한 풍요로움을 창출합니다. 조경 식재, 시설물 설치공사 등 창의적이고 지속 가능한 조경을 추구합니다."
+        "삼성조경은 조경 식재, 시설물 설치공사 등 창의적이고 지속 가능한 조경을 추구합니다."
       }/>
       <meta property="og:type" content="website" />
       <meta property="og:url" content={url || "https://xn--289a529abkak1w.kr"} />
